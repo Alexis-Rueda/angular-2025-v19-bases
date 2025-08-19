0587329d929359d024d0bd321d303e9f2b73111f
alexis.rueda <alexisrueda34@gmail.com>
2025-08-19 02:01:40 -0600

    Agrega el componente DragonballPage con su plantilla y lógica, y actualiza el enrutamiento para incluir la nueva página. También se añade el componente Navbar con enlaces de navegación.

---
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -0,0 +1,7 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  templateUrl: './dragonball-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DragonballPageComponent { }
