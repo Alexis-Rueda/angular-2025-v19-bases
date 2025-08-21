10ec16ac3a5fcb1bbaf9a38b5e2c4f1c5474365b
alexis.rueda <alexisrueda34@gmail.com>
2025-08-20 22:52:59 -0600

    Implementa la lógica para mostrar la lista de personajes en DragonballPage utilizando un @for

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,7 +1,33 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+
+interface Character {
+  id: number;
+  name: string;
+  power: number;
+}
 
 @Component({
   templateUrl: './dragonball-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    // NgClass
+  ]
 })
-export class DragonballPageComponent { }
+export class DragonballPageComponent {
+
+  characters = signal<Character[]>([
+    { id: 1, name: 'Goku', power: 9001 },
+    { id: 2, name: 'Vegeta', power: 9000 },
+    { id: 3, name: 'Gohan', power: 8000 },
+    { id: 4, name: 'Piccolo', power: 2000 },
+    { id: 5, name: 'Frieza', power: 10000 }
+  ]);
+
+  // powerClasses = computed(() => {
+  //   return {
+  //     'text-danger': true
+  //   }
+  // });
+
+}
