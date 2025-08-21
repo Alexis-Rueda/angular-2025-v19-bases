2fe9b56198703ea91f9e0c29195b47963bde8af1
alexis.rueda <alexisrueda34@gmail.com>
2025-08-20 23:23:30 -0600

    Refactoriza el componente DragonballPage para mejorar la gestión de personajes y la lógica de adición de nuevos personajes.

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,5 +1,5 @@
 import { NgClass } from '@angular/common';
-import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 interface Character {
   id: number;
@@ -16,6 +16,10 @@ interface Character {
 })
 export class DragonballPageComponent {
 
+  name = signal('Krilin');
+  power = signal(400);
+
+
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001 },
     { id: 2, name: 'Vegeta', power: 9000 },
@@ -23,6 +27,26 @@ export class DragonballPageComponent {
     { id: 4, name: 'Piccolo', power: 2000 },
     { id: 5, name: 'Frieza', power: 10000 }
   ]);
+
+  addCharacter() {
+    if (!this.name || !this.power() || this.power() <= 0) return;
+
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power()
+    }
+
+    this.characters.update((last) => {
+      return [ ...last, newCharacter ];
+    });
+    this.resetFields();
+  }
+
+  resetFields() {
+    this.name.set('');
+    this.power.set(0);
+  }
 
   // powerClasses = computed(() => {
   //   return {
